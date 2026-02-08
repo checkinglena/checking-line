b22df3ed500f379d2a8dd77677dbf1673d12a71f
Lena <lena@email.com>
2026-02-08 12:00:58 +0100

    working axis with correct date displays

--- tracker.py
+++ tracker.py
@@ -1,11 +1,11 @@
-import numpy as np
 from matplotlib import dates
 from datetime import datetime
-import csv
 from matplotlib import pyplot as plt
+import csv
 import std
 from sys import argv
 
+ref_date = datetime(1970,1,1)
 
 def get_data(file):
     with open(file) as file:
@@ -20,8 +20,9 @@ def get_data(file):
     prices = list(map(float,prices))
     # date parser
     #all_dates = {}
-    ref_date = datetime(2025,12,1)
+    #ref_date = datetime(2025,12,1)
     diff_dates = []
+    instance_dates = []
     # assumes DD-MM-YYYY format for date input (possible separators: -, / or .)
     for x in range(len(dates)):
 
@@ -38,18 +39,33 @@ def get_data(file):
         instance_date = datetime(int(year),int(month),int(day))
         #convert date to number of days passed since reference date
         diff = instance_date - ref_date
+        instance_dates.append(instance_date)
         diff_dates.append(diff)
 
-    diff_days = ([x.days for x in diff_dates])
+    #diff_days = ([x.days for x in diff_dates])
 
     # year = all_dates[x]["day"]
 
-    return diff_days, prices, dates
+
+    return diff_dates, prices
+
+def format_date(date):
+    return date.strftime("%d-%m-%y")
+
+
 
 def plot_data(file):
-    diff_days, prices, raw_dates = get_data(file)
-    std.default.plt_pretty("days since 01-12-2025","unit price")
+    diff_dates, prices = get_data(file)
+    diff_days = ([x.days for x in diff_dates]) # list of timedelta objects
+
+    #formatted_inst_dates = list(map(format_date,inst_dates))
+    #xvals = lambda x: [map(format_date,inst_dates[day]) for day in range(len(x))]
+
+    formatter = dates.DateFormatter('%d-%m-%Y')
+
+    std.default.plt_pretty("date","unit price")
     plt.scatter(diff_days,prices)
+    plt.gca().xaxis.set_major_formatter(formatter)
     plt.show()
 
 
